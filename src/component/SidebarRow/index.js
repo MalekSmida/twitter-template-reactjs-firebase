40a5775595c81efa4771f216f6338ff66cc628a8
Malek Smida <msmida96@gmail.com>
2020-09-26 12:50:52 +0100

    @init project

---
+++ src/component/SidebarRow/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "/index.scss";
+
+function SidebarRow() {
+  return (
+    <div className="sidebarRow">
+      <h1>SidebarRow</h1>
+    </div>
+  );
+}
+
+export default SidebarRow;
