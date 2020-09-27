322811dfab1c45b10cc2977086aaf7c0ec1cb31b
Malek Smida <msmida96@gmail.com>
2020-09-27 22:09:03 +0100

    #Add: Post component

--- src/component/Feed/index.js
+++ src/component/Feed/index.js
@@ -10,40 +10,27 @@ function Feed() {
         <h2>Home</h2>
       </div>
       <TweetBox />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        username="malek_smida"
+        displayName="Malek Smida"
+        text="Where is my car!!"
+        image="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"
+        avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
+      />
+      <Post
+        username="malek_smida"
+        displayName="Malek Smida"
+        text="Where is my car!!"
+        image="https://images.unsplash.com/photo-1524678714210-9917a6c619c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"
+        avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
+      />
+      <Post
+        username="malek_smida"
+        displayName="Malek Smida"
+        text="Where is my car!!"
+        image="https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
+      />
     </div>
   );
 }
--- src/component/Post/index.js
+++ src/component/Post/index.js
@@ -1,9 +1,49 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
+import "./index.scss";
 
-function Post() {
+function Post({
+  displayName,
+  username,
+  verified,
+  timestamp,
+  text,
+  image,
+  avatar,
+}) {
   return (
     <div className="post">
-      <h1>post</h1>
+      <div className="post__avatar">
+        <Avatar src={avatar} />
+      </div>
+      <div className="post__body">
+        <div className="post__body__header">
+          <div className="post__body__headerText">
+            <h3>
+              {displayName}
+              <span className="post__body__headerSpecial">
+                <VerifiedUserIcon className="post__body__badge" />
+                {username}
+              </span>
+            </h3>
+          </div>
+          <div className="post__body__headerDescription">
+            <p>{text}</p>
+          </div>
+        </div>
+        <img src={image} />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
+        </div>
+      </div>
     </div>
   );
 }
