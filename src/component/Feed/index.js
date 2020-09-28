a88dc981b423572bd08f06d072bb50a8616d6cee
Malek Smida <msmida96@gmail.com>
2020-09-28 17:20:27 +0100

    #Edit: feed

--- src/component/Feed/index.js
+++ src/component/Feed/index.js
@@ -13,22 +13,22 @@ function Feed() {
       <Post
         username="malek_smida"
         displayName="Malek Smida"
-        text="Where is my car!!"
+        text="Good morning!"
         image="https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"
         avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
       />
       <Post
         username="malek_smida"
         displayName="Malek Smida"
-        text="Where is my car!!"
+        text="Time is a valuable asset!"
         image="https://images.unsplash.com/photo-1524678714210-9917a6c619c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"
         avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
       />
       <Post
         username="malek_smida"
         displayName="Malek Smida"
-        text="Where is my car!!"
-        image="https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        text="tweet!"
+        image="https://venturebeat.com/wp-content/uploads/2016/02/Taco-Party.gif?resize=800%2C400&strip=all"
         avatar="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
       />
     </div>
