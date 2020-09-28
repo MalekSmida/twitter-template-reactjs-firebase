d12a989cdeeae14a9579a77479301b46e61f0128
Malek Smida <msmida96@gmail.com>
2020-09-28 17:13:14 +0100

    #Add: Widgets component

--- src/component/Widgets/index.js
+++ src/component/Widgets/index.js
@@ -1,9 +1,32 @@
 import React from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
+import "./index.scss";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h1>widgets</h1>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>Trends for you</h2>
+        <TwitterTweetEmbed tweetId={"1050118621198921728"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Twitter"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://www.facebook.com/malek.smida.54540/"}
+          options={{ text: "#reactjs is awesome", via: "maleksmida" }}
+        />
+      </div>
     </div>
   );
 }
