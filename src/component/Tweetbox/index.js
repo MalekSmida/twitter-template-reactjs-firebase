ea7beb2ca1cc80676a3d1b14569f61bc2b9ab33d
Malek Smida <msmida96@gmail.com>
2020-09-28 17:34:28 +0100

    #Edit: Tweetbox style

--- src/component/Tweetbox/index.js
+++ src/component/Tweetbox/index.js
@@ -12,12 +12,12 @@ function TweetBox() {
             src="https://scontent.ftun6-1.fna.fbcdn.net/v/t1.0-9/83020011_725652291572674_1450846223241052160_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=n2Zeto3D8qkAX-9jz7Q&_nc_ht=scontent.ftun6-1.fna&oh=df464cdbcb44b26d2924642c597b63c8&oe=5F8D5D6B"
           />
           <input placeholder="What's happening?" type="text" />
+          <input
+            className="tweetBox__imageInput"
+            placeholder="Enter image URL"
+            type="text"
+          />
         </div>
-        <input
-          className="tweetBox__imageInput"
-          placeholder="Enter image URL"
-          type="text"
-        />
         <Button className="tweetBox__button">Tweet</Button>
       </form>
     </div>
--- src/component/Widgets/index.js
+++ src/component/Widgets/index.js
@@ -17,11 +17,11 @@ function Widgets() {
       <div className="widgets__widgetContainer">
         <h2>Trends for you</h2>
         <TwitterTweetEmbed tweetId={"1050118621198921728"} />
-        <TwitterTimelineEmbed
+        {/* <TwitterTimelineEmbed
           sourceType="profile"
           screenName="Twitter"
           options={{ height: 400 }}
-        />
+        /> */}
         <TwitterShareButton
           url={"https://www.facebook.com/malek.smida.54540/"}
           options={{ text: "#reactjs is awesome", via: "maleksmida" }}
